11accb51e9d0cff5c73c67f8a25352024415842c
Maximiliano <stuckeedd@gmail.com>
2026-05-02 19:57:44 -0300

    feat: connect login and dashboard to API

--- src/services/products.js
+++ src/services/products.js
@@ -1,11 +1,9 @@
 import { API_URL } from './api'
 
-export async function getProducts(token) {
-    const response = await fetch (`${API_URL}/products/`, {
-        headers: { Authorization: `Bearer ${token}` }
-    })
-    if(!response.ok) throw new Error('Error al obtener productos')
-    return response.json()
+export async function getProducts() {
+  const res = await fetch(`${API_URL}/products/`)
+  if (!res.ok) throw new Error('Error al obtener productos')
+  return res.json()
 }
 
 
